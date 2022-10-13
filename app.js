ede7ffd58aca0d54c912d730155841bed769ec6a
Raquel Olaya <raquelolaya@Raquels-MacBook-Air.local>
2022-10-12 20:49:38 -0500

    buttons and display user choice

---
+++ app.js
@@ -0,0 +1,9 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id 
+    userChoiceDisplay.innerHTML = userChoice
+}))
